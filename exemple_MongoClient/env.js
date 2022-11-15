b1c2117342435dd7adb1357ff0e316d2fe92d182
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2022-11-15 12:36:11 -0500

    Removed old files and moved package.json to root

--- exemple_MongoClient/env.js
+++
@@ -1,8 +0,0 @@
-const DB_DB = "database";
-const DB_COLLECTION = "classes";
-// TODO : fournir les bonnes informations de connexion (les <> doivent être enlevés)
-const DB_URL = "mongodb+srv://<username>:<passwordHere>@log2440cluster.fzyah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-exports.DB_DB = DB_DB;
-exports.DB_COLLECTION = DB_COLLECTION;
-exports.DB_URL = DB_URL;
